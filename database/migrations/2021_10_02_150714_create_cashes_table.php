88d249f1f8b16cb555365de16d7363685f23f674
Fozimat <fozimata@gmail.com>
2021-10-02 22:19:38 +0700

    Model and migration

---
+++ database/migrations/2021_10_02_150714_create_cashes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCashesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cashes', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->date('date');
+            $table->bigInteger('total');
+            $table->integer('type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cashes');
+    }
+}
