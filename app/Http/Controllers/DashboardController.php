30fd573b1e94a4741b5cbcabf9602080f154a060
Fozimat <fozimata@gmail.com>
2021-10-04 20:58:40 +0700

    ok

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cash;
-use Illuminate\Http\Request;
+use App\Models\Type;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -13,7 +14,15 @@ class DashboardController extends Controller
         $kas_keluar = Cash::where('type_id', 2)->sum('total');
         $kas = $kas_masuk - $kas_keluar;
         $total_data = Cash::count();
-        $kas_terbaru = Cash::with(['type'])->orderBy('date', 'ASC')->limit(5)->get();
+        $kas_terbaru = Cash::with(['type'])->orderBy('date', 'DESC')->limit(5)->get();
+
+        $tabel = Cash::select(DB::raw("types.type as label, SUM(total) as value"))
+            ->join('types', 'cashes.type_id', '=', 'types.id')
+            ->groupBy('type_id')
+            ->get();
+
+        // dd($tabel->toJson(JSON_PRETTY_PRINT));
+
         return view('pages.dashboard.index', compact(['kas_masuk', 'kas_keluar', 'kas', 'total_data', 'kas_terbaru']));
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -108,9 +108,21 @@
         </div>
     </div>
 </div>
+
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>LINE CHART</h2>
+            </div>
+            <div class="body">
+                <canvas id="line_chart" height="100"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 @push('after-script')
-
 <script src="{{ asset('assets/plugins/chartjs/Chart.bundle.js') }}"></script>
 <script>
     var ctx = document.getElementById("pie_chart").getContext('2d');
@@ -130,9 +142,46 @@
                 ]
             },
             options: {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                              
+                            }
+                        }
+                    }
+                }
+            }
+		});
+
+        var line = document.getElementById("line_chart").getContext('2d');
+		var myChart = new Chart(line, {
+			type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    borderColor: 'rgba(0, 188, 212, 0.75)',
+                    backgroundColor: 'rgba(0, 188, 212, 0.3)',
+                    pointBorderColor: 'rgba(0, 188, 212, 0)',
+                    pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
+                    pointBorderWidth: 1
+                }, {
+                        label: "My Second dataset",
+                        data: [28, 48, 40, 19, 86, 27, 90],
+                        borderColor: 'rgba(233, 30, 99, 0.75)',
+                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
+                        pointBorderColor: 'rgba(233, 30, 99, 0)',
+                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
+                        pointBorderWidth: 1
+                    }]
+            },
+            options: {
                 responsive: true,
                 legend: false
             }
 		});
+
 </script>
 @endpush
