81c259f0ac747c8fe28f133ee82f31e9c8746a8b
Fozimat <fozimata@gmail.com>
2021-10-06 11:40:30 +0700

    Icon menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,7 +9,7 @@
         </li>
         <li class="{{ request()->is('pemasukan*') ? 'active' : '' }}">
             <a href="{{ route('pemasukan.index') }}">
-                <i class="material-icons">text_fields</i>
+                <i class="material-icons">monetization_on</i>
                 <span>Kas Masuk</span>
             </a>
         </li>
@@ -21,29 +21,24 @@
         </li>
 
         <li class="header">LABELS</li>
-        <li>
-            <a href="#">
-                <i class="material-icons col-red">donut_large</i>
-                <span>Rekapitulasi</span>
-            </a>
-        </li>
+
         <li class="{{ request()->is('laporan*') ? 'active' : '' }}">
             <a href="{{ route('laporan') }}">
-                <i class="material-icons col-amber">donut_large</i>
+                <i class="material-icons">print</i>
                 <span>Cetak Laporan</span>
             </a>
         </li>
         <li class="header">SETTINGS</li>
         <li>
             <a href="#">
-                <i class="material-icons col-light-blue">donut_large</i>
+                <i class="material-icons">person</i>
                 <span>Profile</span>
             </a>
         </li>
         <li>
             <a href="{{ route('logout') }}" href="{{ route('logout') }}" onclick="event.preventDefault();
                           document.getElementById('logout-form').submit();">
-                <i class="material-icons col-red">donut_large</i>
+                <i class="material-icons">subdirectory_arrow_right</i>
                 <span>Logout</span>
             </a>
         </li>
